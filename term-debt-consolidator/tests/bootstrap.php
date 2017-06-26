3078eaef123caa74d2637c1ba54ed8858bb7dcd7
rclations <contact@rclations.com>
2017-06-26 18:21:47 -0400

    adding boilerplate

---
+++ term-debt-consolidator/tests/bootstrap.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Term_Debt_Consolidator Test Bootstrapper.
+ *
+ * @since   1.0.0
+ * @package Term_Debt_Consolidator
+ */
+
+// Get our tests directory.
+$_tests_dir = ( getenv( 'WP_TESTS_DIR' ) ) ? getenv( 'WP_TESTS_DIR' ) : '/tmp/wordpress-tests-lib';
+
+// Include our tests functions.
+require_once $_tests_dir . '/includes/functions.php';
+
+/**
+ * Manually require our plugin for testing.
+ *
+ * @since 1.0.0
+ */
+function _manually_load_term_debt_consolidator_plugin() {
+
+	// Include the REST API main plugin file if we're using it so we can run endpoint tests.
+	if ( class_exists( 'WP_REST_Controller' ) && file_exists( WP_PLUGIN_DIR . '/rest-api/plugin.php' ) ) {
+		require WP_PLUGIN_DIR . '/rest-api/plugin.php';
+	}
+
+	// Require our plugin.
+	require dirname( dirname( __FILE__ ) ) . '/term-debt-consolidator.php';
+}
+
+// Inject in our plugin.
+tests_add_filter( 'muplugins_loaded', '_manually_load_term_debt_consolidator_plugin' );
+
+// Include the main tests bootstrapper.
+require $_tests_dir . '/includes/bootstrap.php';
