b791d126ffb2e7bc4bda25398f94ec19d40551d0
rclations <contact@rclations.com>
2017-06-26 18:25:13 -0400

    add basic options page

---
+++ term-debt-consolidator/tests/test-admin.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Term Debt Consolidator Admin Tests.
+ *
+ * @since   1.0.0
+ * @package Term_Debt_Consolidator
+ */
+class TDC_Admin_Test extends WP_UnitTestCase {
+
+	/**
+	 * Test if our class exists.
+	 *
+	 * @since  1.0.0
+	 */
+	function test_class_exists() {
+		$this->assertTrue( class_exists( 'TDC_Admin') );
+	}
+
+	/**
+	 * Test that we can access our class through our helper function.
+	 *
+	 * @since  1.0.0
+	 */
+	function test_class_access() {
+		$this->assertInstanceOf( 'TDC_Admin', term_debt_consolidator()->admin );
+	}
+
+	/**
+	 * Replace this with some actual testing code.
+	 *
+	 * @since  1.0.0
+	 */
+	function test_sample() {
+		$this->assertTrue( true );
+	}
+}
