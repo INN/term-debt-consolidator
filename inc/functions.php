4e15360374a1a0d41ee8eda187c6de49d86206d0
rclations <contact@rclations.com>
2017-06-26 23:05:51 -0400

    add script/style enqueue for options page

--- inc/functions.php
+++ inc/functions.php
@@ -1,21 +1,6 @@
 <?php
 
 /**
- * Render a template by specifying a filename and context.
- *
- * @param (string) $template -- the filename of the template to render.
- * @param (array) $context -- associative array of values used within the template.
- *
- * @since 0.1
- */
-function tdc_render_template($template, $context=false) {
-	if (!empty($context))
-		extract($context);
-
-	include TDC_TEMPLATE_DIR . '/' . $template;
-}
-
-/**
  * Returns a JSON object with some of the essential bits used in the front-end javascript
  *
  * @since 0.1
