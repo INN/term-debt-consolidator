6abf82cd50ab5e9663bb8d9697d081a9716a6902
rclations <contact@rclations.com>
2017-06-29 16:26:20 -0400

    add post type + endpoint for storing recommendations

---
+++ includes/class-post-type.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Term Debt Consolidator Post Type.
+ *
+ * @since   1.0.0
+ * @package Term_Debt_Consolidator
+ */
+
+/**
+ * Term Debt Consolidator Post Type.
+ *
+ * @since 1.0.0
+ */
+class TDC_Post_Type {
+	/**
+	 * Parent plugin class.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var   Term_Debt_Consolidator
+	 */
+	protected $plugin = null;
+
+	/**
+	 * Constructor.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @param  Term_Debt_Consolidator $plugin Main plugin object.
+	 */
+	public function __construct( $plugin ) {
+		$this->plugin = $plugin;
+		$this->hooks();
+	}
+
+	/**
+	 * Initiate our hooks.
+	 *
+	 * @since  1.0.0
+	 */
+	public function hooks() {
+		add_action( 'init', array( $this, 'term_debt_consolidator_recommendations' ) );
+	}
+
+	// Register Custom Post Type
+	public function term_debt_consolidator_recommendations() {
+
+		$labels = array(
+			'name'                  => _x( 'Term Debt Consolidator Recommendations', 'term_debt_consolidator' ),
+		);
+		$args = array(
+			'label'                 => __( 'Term Debt Consolidator Recommendations', 'term_debt_consolidator' ),
+			'labels'                => $labels,
+			'supports'              => array( ),
+			'taxonomies'            => array( 'category', 'post_tag' ),
+			'hierarchical'          => false,
+			'public'                => false,
+			'show_in_rest'          => true,
+			'show_ui'               => false,
+			'show_in_menu'          => false,
+			'show_in_admin_bar'     => false,
+			'show_in_nav_menus'     => false,
+			'can_export'            => false,
+			'has_archive'           => false,
+			'exclude_from_search'   => true,
+			'publicly_queryable'    => true,
+			'capability_type'       => 'page',
+		);
+		register_post_type( 'term_consolidate_rec', $args );
+
+	}
+}
---
+++ tests/test-post-type.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Term Debt Consolidator Post Type Tests.
+ *
+ * @since   1.0.0
+ * @package Term_Debt_Consolidator
+ */
+class TDC_Post_Type_Test extends WP_UnitTestCase {
+
+	/**
+	 * Test if our class exists.
+	 *
+	 * @since  1.0.0
+	 */
+	function test_class_exists() {
+		$this->assertTrue( class_exists( 'TDC_Post_Type' ) );
+	}
+
+	/**
+	 * Test that we can access our class through our helper function.
+	 *
+	 * @since  1.0.0
+	 */
+	function test_class_access() {
+		$this->assertInstanceOf( 'TDC_Post_Type', term_debt_consolidator()->post-type );
+	}
+
+	/**
+	 * Replace this with some actual testing code.
+	 *
+	 * @since  1.0.0
+	 */
+	function test_sample() {
+		$this->assertTrue( true );
+	}
+}
