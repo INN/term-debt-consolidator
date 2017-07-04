4e7f6e8cdcb54c18a4f1bc304a57ddc40c140385
rclations <contact@rclations.com>
2017-07-04 02:05:40 -0400

    cleaning up tests

--- tests/test-plugin-list-table.php
+++ tests/test-plugin-list-table.php
@@ -8,24 +8,6 @@
 class TDC_Plugin_List_Table_Test extends WP_UnitTestCase {
 
 	/**
-	 * Test if our class exists.
-	 *
-	 * @since  1.0.0
-	 */
-	function test_class_exists() {
-		$this->assertTrue( class_exists( 'TDC_Plugin_List_Table' ) );
-	}
-
-	/**
-	 * Test that we can access our class through our helper function.
-	 *
-	 * @since  1.0.0
-	 */
-	function test_class_access() {
-		$this->assertInstanceOf( 'TDC_Plugin_List_Table', term_debt_consolidator()->plugin_list_table );
-	}
-
-	/**
 	 * Replace this with some actual testing code.
 	 *
 	 * @since  1.0.0
